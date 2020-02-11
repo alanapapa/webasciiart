1c73e478182d391735a1c140647b5418360d0fd6
alanapapa <berikbazar@yandex.com>
2020-02-11 22:12:39 +0600

    fix

--- ascii-art-web.go
+++ ascii-art-web.go
@@ -10,13 +10,19 @@ import (
 	"strings"
 )
 
-var text string
-var fonts string
-var Result string
+var text, fonts, Result string
+
+func main() {
+	http.HandleFunc("/", hello)
+	fmt.Printf("Starting server for testing HTTP POST...\n http://127.0.0.1:1995/ \n")
+	if err := http.ListenAndServe(":1995", nil); err != nil {
+		log.Fatal(err)
+	}
+}
 
 func hello(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-		http.Error(w, "404 not found.", http.StatusNotFound)
+		http.Error(w, "Error 404\nPage Not Found", http.StatusNotFound)
 		return
 	}
 
@@ -24,71 +30,35 @@ func hello(w http.ResponseWriter, r *http.Request) {
 	case "GET":
 		http.ServeFile(w, r, "static")
 	case "POST":
-		// Call ParseForm() to parse the raw query and update r.PostForm and r.Form.
-		if err := r.ParseForm(); err != nil {
-			fmt.Fprintf(w, "ParseForm() err: %v", err)
+		fonts = r.FormValue("fonts")
+		text = r.FormValue("text")
+
+		for _, e := range text {
+			if e < 32 || e > 126 {
+				http.Error(w, "Error 400\nBad Request!", http.StatusNotFound)
+				return
+			}
+		}
+
+		file, err := os.Open("./" + fonts + ".txt")
+
+		if err != nil {
+			http.Error(w, "Internal server error - 500", http.StatusNotFound)
 			return
 		}
 
-		fonts = r.FormValue("fonts")
-		text = r.FormValue("text")
-		AsciiMain(text, fonts)
+		fileVal := ScanFile(file)
+		narg := strings.Split(text+" ", "\\n")
+		for _, v := range narg {
+			goResult(string(v), fileVal)
+		}
+
 		tmpl, _ := template.ParseFiles("static/index.html")
 		tmpl.Execute(w, Result)
-	default:
-		fmt.Fprintf(w, "Sorry, only GET and POST methods are supported.")
 	}
 }
 
-func main() {
-
-	http.HandleFunc("/", hello)
-
-	fmt.Printf("Starting server for testing HTTP POST...\n http://127.0.0.1:1995/ \n")
-	if err := http.ListenAndServe(":1995", nil); err != nil {
-		log.Fatal(err)
-	}
-}
-
-func AsciiMain(text string, fonts string) {
-	for _, e := range text {
-		if e == '§' || e == '±' || e == '”' || e == '“' {
-			text = "Lol Kek Cheburek"
-		}
-	}
-	if fonts == "Thinkertoy" {
-		file, _ := os.Open("thinkertoy.txt")
-		fileVal := ScanFile(file)
-		arg := text + " "
-		narg := strings.Split(arg, "\\n")
-		for _, v := range narg {
-			printLetter(string(v), fileVal)
-		}
-	} else if fonts == "Shadow" {
-		file, _ := os.Open("shadow.txt")
-		fileVal := ScanFile(file)
-		arg := text + " "
-		narg := strings.Split(arg, "\\n")
-		for _, v := range narg {
-			printLetter(string(v), fileVal)
-		}
-	} else if fonts == "Standard" {
-		file, _ := os.Open("standard.txt")
-		fileVal := ScanFile(file)
-		arg := text + " "
-		narg := strings.Split(arg, "\\n")
-		for _, v := range narg {
-			printLetter(string(v), fileVal)
-		}
-	} else {
-		fmt.Println("Problem")
-	}
-
-	return
-
-}
-
-func printLetter(s string, fileVal []string) string {
+func goResult(s string, fileVal []string) string {
 	for i := 1; i <= 8; i++ {
 		for _, arg := range s {
 			indexBase := int(rune(arg)-32) * 9
@@ -99,10 +69,10 @@ func printLetter(s string, fileVal []string) string {
 	return Result
 }
 
-func ScanFile(arg *os.File) []string {
+func ScanFile(font *os.File) []string {
 	Result = ""
 	var fileValue []string
-	scanner := bufio.NewScanner(arg)
+	scanner := bufio.NewScanner(font)
 	for scanner.Scan() {
 		lines := scanner.Text()
 		fileValue = append(fileValue, lines)
